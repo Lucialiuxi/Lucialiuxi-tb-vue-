22ff8fd9189182db3db36c9b2184c2ea54ca218c
Lucialiuxi <1435554190@qq.com>
2022-01-02 15:38:10 +0800

    layout

---
+++ tb-vue/src/components/layout/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="main-page">
+        <!-- 头部导航栏 -->
+        <div class="nav-bar-wrap">
+            <div class="nav-bar">
+                <div class="left-area">
+                    <i class="el-icon-search"></i>
+                    <el-select
+                        v-model="value"
+                        multiple
+                        filterable
+                        remote
+                        reserve-keyword
+                        clearable
+                        placeholder="搜索个人项目"
+                        :remote-method="remoteMethod"
+                        :loading="loading"
+                        class="search-file-box"
+                        size="small"
+                    >
+                        <el-option
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                        >
+                        </el-option>
+                    </el-select>
+                </div>
+                <div class="right-area">
+                    <span class="greeting-text">你好, {{userInfo.username}} ~~</span>
+                    <el-button type="text" size="medium">登出</el-button>
+                    <el-avatar icon="el-icon-user-solid"></el-avatar>
+                </div>
+            </div>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { Session } from '@/utils/storage';
+export default {
+    name: "layout",
+    data() {
+        return {
+            options: [],
+            loading: false,
+            value: [],
+            userInfo: {},
+        }
+    },
+    created(){
+        this.userInfo = Session.get('userInfo');
+    },
+    methods: {
+        remoteMethod(query: string){
+            console.log(query)
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import './layout.scss';
+</style>
--- tb-vue/src/views/file/index.vue
+++ tb-vue/src/views/file/index.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>12312312</div>
+    <layout>
+        <template v-slot: default>
+        </template>
+    </layout>
 </template>
 
 <script lang="ts">
+import layout from '@/components/layout/index.vue';
 
 export default {
     name: 'File',
+    components: {
+        layout,
+    },
     data() {
         return {
             a: 123
